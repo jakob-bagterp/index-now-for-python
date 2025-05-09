7d2a3c02ef25b9959819323b349d86a76aa690b2
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-09 20:25:08 +0200

    Fixed documentation that caused issue with requests: "requests.exceptions.MissingSchema: Invalid URL '***' No scheme supplied."

--- src/index_now/authentication.py
+++ src/index_now/authentication.py
@@ -6,7 +6,7 @@ class IndexNowAuthentication:
     """Authentication data for the IndexNow API.
 
     Args:
-        host (str): The host of the website to be indexed, e.g. `example.com`.
+        host (str): The host of the website to be indexed, e.g. `https://example.com`.
         api_key (str): The IndexNow API key, e.g. `a1b2c3d4`.
         api_key_location (str): The URL of the IndexNow API key file, e.g. `https://example.com/a1b2c3d4.txt`.
     """

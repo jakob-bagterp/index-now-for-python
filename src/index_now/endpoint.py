76b0aafbe0dce4350b157ccd44f07f63d12f2034
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-11 01:20:16 +0200

    Fixed URL entry points (reference: https://www.indexnow.org/faq).

--- src/index_now/endpoint.py
+++ src/index_now/endpoint.py
@@ -5,12 +5,12 @@ from enum import Enum, unique
 class SearchEngineEndpoint(Enum):
     """Endpoint options for the IndexNow API. Reference: https://www.indexnow.org/faq"""
 
-    INDEXNOW = "https://api.indexnow.org/"
-    MICROSOFT_BING = "https://www.bing.com/"
+    INDEXNOW = "https://api.indexnow.org/indexnow"
+    MICROSOFT_BING = "https://www.bing.com/indexnow"
     NAVER = "https://searchadvisor.naver.com/indexnow"
     SEZNAM = "https://search.seznam.cz/indexnow"
-    YANDEX = "https://yandex.com/"
-    YEP = "https://indexnow.yep.com/"
+    YANDEX = "https://yandex.com/indexnow"
+    YEP = "https://indexnow.yep.com/indexnow"
 
     def __str__(self) -> str:
         return str(self.value)

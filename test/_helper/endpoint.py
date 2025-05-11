f824ddfc41a370a56a4cdb512cceae24156787e3
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-11 14:40:23 +0200

    Added skip of Bing endpoint temporarily as it's now accepting submissions.

--- test/_helper/endpoint.py
+++ test/_helper/endpoint.py
@@ -2,6 +2,10 @@ import requests
 
 from index_now import SearchEngineEndpoint
 
+TEMPORARILY_SKIPPED_ENDPOINTS = [
+    SearchEngineEndpoint.MICROSOFT_BING,
+]
+
 
 def is_endpoint_up(endpoint: SearchEngineEndpoint | str) -> bool:
     """According to the IndexNow documentation for search engines (https://www.indexnow.org/searchengines), the endpoint should always host a `meta.json` file, so this is a proxy to test whether the endpoint is up. Find a list of endpoints here: https://www.indexnow.org/searchengines.json"""

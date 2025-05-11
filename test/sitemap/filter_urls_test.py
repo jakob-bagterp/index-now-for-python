48f19044aecbf00701105e928222ac4bf7ec02b9
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-11 11:52:01 +0200

    Updated test of filter_url().

--- test/sitemap/filter_urls_test.py
+++ test/sitemap/filter_urls_test.py
@@ -1,17 +1,15 @@
 import pytest
+from _helper.sitemap import get_mock_sitemap_content
 
-from index_now.sitemap import filter_urls
+from index_now.sitemap import filter_urls, parse_sitemap_xml_and_get_urls
 
-TEST_URLS = [
-    "https://example.com/page1",
-    "https://example.com/page2",
-    "https://example.com/page3",
-]
+TEST_URLS = parse_sitemap_xml_and_get_urls(get_mock_sitemap_content())
 
 
 @pytest.mark.parametrize("urls, contains, skip, take, expected", [
     (TEST_URLS, None, None, None, TEST_URLS),
+    (TEST_URLS, None, 0, None, TEST_URLS),
 ])
-def test_filter_urls(urls: list[str], contains: str | None, skip: int | None, take: int | None, expected: list[str]):
+def test_filter_urls(urls: list[str], contains: str | None, skip: int | None, take: int | None, expected: list[str]) -> None:
     result = filter_urls(urls, contains)
     assert result == expected

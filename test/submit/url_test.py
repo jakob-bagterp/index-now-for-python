17ca863b1aae3079771605943dc66bb4230f8857
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-11 03:25:53 +0200

    Fixed various tests.

--- test/submit/url_test.py
+++ test/submit/url_test.py
@@ -6,9 +6,11 @@ from index_now import SearchEngineEndpoint, submit_url_to_index_now
 
 
 def test_submit_url_to_index_now(capfd: object) -> None:
-    submit_url_to_index_now(TIMER_FOR_PYTHON.authentication, "https://jakob-bagterp.github.io/timer-for-python/", endpoint=SearchEngineEndpoint.INDEXNOW)
+    endpoint = SearchEngineEndpoint.INDEXNOW
+    submit_url_to_index_now(TIMER_FOR_PYTHON.authentication, "https://jakob-bagterp.github.io/timer-for-python/", endpoint=endpoint)
     terminal_output, _ = capfd.readouterr()
-    assert f"Status code: {Color.RED}422{Color.OFF}" in terminal_output
+    assert f"{Color.GREEN}URL submitted successfully to the IndexNow API:{Color.OFF} {endpoint}" in terminal_output
+    assert f"Status code: {Color.GREEN}200{Color.OFF}" or f"Status code: {Color.GREEN}202{Color.OFF}" in terminal_output
 
 
 ENDPOINT_EXCEPTIONS = [
@@ -22,6 +24,5 @@ ENDPOINT_EXCEPTIONS = [
 def test_submit_url_to_various_search_engines(endpoint: SearchEngineEndpoint, capfd: object) -> None:
     submit_url_to_index_now(TIMER_FOR_PYTHON.authentication, "https://jakob-bagterp.github.io/timer-for-python/", endpoint=endpoint)
     terminal_output, _ = capfd.readouterr()
-    if endpoint in ENDPOINT_EXCEPTIONS:
-        pytest.skip(f"Endpoint {endpoint} gives different status code than expected.")
-    assert f"Status code: {Color.RED}422{Color.OFF}" in terminal_output
+    assert f"{Color.GREEN}URL submitted successfully to the IndexNow API:{Color.OFF} {endpoint}" in terminal_output
+    assert f"Status code: {Color.GREEN}200{Color.OFF}" or f"Status code: {Color.GREEN}202{Color.OFF}" in terminal_output

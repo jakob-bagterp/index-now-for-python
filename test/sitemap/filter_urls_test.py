7466b91b847b80f7b3928e6561ffbed42c8f6e46
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-11 14:15:32 +0200

    Fixed tests of filter_urls() and added more tests.

--- test/sitemap/filter_urls_test.py
+++ test/sitemap/filter_urls_test.py
@@ -9,7 +9,15 @@ TEST_URLS = parse_sitemap_xml_and_get_urls(get_mock_sitemap_content())
 @pytest.mark.parametrize("urls, contains, skip, take, expected", [
     (TEST_URLS, None, None, None, TEST_URLS),
     (TEST_URLS, None, 0, None, TEST_URLS),
+    (TEST_URLS, None, None, 0, []),
+    (TEST_URLS, None, 1, None, TEST_URLS[1:]),
+    (TEST_URLS, None, len(TEST_URLS), None, []),
+    (TEST_URLS, None, 0, 1, TEST_URLS[0:1]),
+    (TEST_URLS, "page1", None, None, [TEST_URLS[i] for i in [1, 5, 7]]),
+    (TEST_URLS, "page1", None, 1, [TEST_URLS[1]]),
+    (TEST_URLS, r"(page1)|(section1)", None, None, [TEST_URLS[i] for i in [1, 5, 6, 7]]),
+    (TEST_URLS, r"(page1)|(section1)", 1, 2, [TEST_URLS[i] for i in [5, 6]]),
 ])
 def test_filter_urls(urls: list[str], contains: str | None, skip: int | None, take: int | None, expected: list[str]) -> None:
-    result = filter_urls(urls, contains)
-    assert result == expected
+    filtered_urls = filter_urls(urls, contains, skip, take)
+    assert filtered_urls == expected

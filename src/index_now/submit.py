cc351d21e9e18283d1b1c13ca74b50910480f79d
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-13 13:31:45 +0200

    Minor edit of example.

--- src/index_now/submit.py
+++ src/index_now/submit.py
@@ -54,8 +54,8 @@ def submit_urls_to_index_now(authentication: IndexNowAuthentication, urls: list[
     """Submits a list of URLs to the IndexNow API of a search engine.
 
     Args:
-        authentication (IndexNowAuthentication): Authentication data for the IndexNow API.
-        urls (list[str]): List of URLs to submit, e.g. `["https://example.com/page1", "https://example.com/page2"]`.
+        authentication (IndexNowAuthentication): Authentication credentials for the IndexNow API.
+        urls (list[str]): List of URLs to submit. For example: `["https://example.com/page1", "https://example.com/page2", "https://example.com/page3"]`
         endpoint (SearchEngineEndpoint | str, optional): Select the search engine you want to submit to or use a custom URL as endpoint.
 
     Example:

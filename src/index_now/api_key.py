1c17fc6dddebdb9ed607bc1294da3b87535ba411
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-22 20:37:36 +0200

    Added reference documentation for generate_api_key() method.

--- src/index_now/api_key.py
+++ src/index_now/api_key.py
@@ -11,6 +11,42 @@ def generate_api_key(length: int = 32) -> str:
 
     Returns:
         str: An 8 to 128 character hexadecimal string, e.g. `5017988d51af458491d21ecab6ed1811` for a length of 32 characters.
+
+    Example:
+        How to generate a random API key:
+
+        ```python linenums="1" hl_lines="3"
+        from index_now import generate_api_key
+
+        api_key = generate_api_key()
+
+        print(api_key)
+        ```
+
+        This will print a random API key of 32 characters. Example:
+
+        ```bash title=""
+        5017988d51af458491d21ecab6ed1811
+        ```
+
+        How to generate a random API key with a custom length:
+
+        ```python linenums="1" hl_lines="3-4"
+        from index_now import generate_api_key
+
+        api_key_16 = generate_api_key(length=16)
+        api_key_64 = generate_api_key(length=64)
+
+        print(api_key_16)
+        print(api_key_64)
+        ```
+
+        This will print two random API keys of 16 and 64 characters. Example:
+
+        ```bash title=""
+        5017988d51af4584
+        5017988d51af458491d21ecab6ed18115017988d51af458491d21ecab6ed1811
+        ```
     """
 
     if not 8 <= length <= 128:

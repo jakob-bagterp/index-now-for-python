efc5f03a84520c8c284fedcef100dc4601dc7d58
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-22 19:51:20 +0200

    Updated length output of generator and added extra tests.

--- src/index_now/api_key.py
+++ src/index_now/api_key.py
@@ -1,3 +1,5 @@
+import random
+import string
 import uuid
 
 
@@ -8,10 +10,12 @@ def generate_api_key(length: int = 32) -> str:
         length (int, optional): Length of the API key. Should be minimum 8 and maximum 128.
 
     Returns:
-        str: A 32 character hexadecimal string, e.g. `5017988d51af458491d21ecab6ed1811`.
+        str: An 8 to 128 character hexadecimal string, e.g. `5017988d51af458491d21ecab6ed1811` for a length of 32 characters.
     """
 
     if not 8 <= length <= 128:
         raise ValueError("Length must be between 8 and 128.")
 
-    return str(uuid.uuid4()).replace("-", "")[:length]
+    if length <= 32:
+        return str(uuid.uuid4()).replace("-", "")[:length]
+    return "".join(random.choices(string.ascii_lowercase + string.digits, k=length))
--- test/api_key/generate_test.py
+++ test/api_key/generate_test.py
@@ -1,10 +1,25 @@
 import re
 
+import pytest
+
 from index_now import generate_api_key
 
 
-def test_generate_random_api_key() -> None:
-    api_key_pattern = re.compile(r"^[0-9a-f]{32}$")
+def compile_regex_pattern(length: int) -> re.Pattern[str]:
+    """Compile a regex pattern for the API key based on the length. Adheres to the IndexNow speficication: https://www.indexnow.org/documentation"""
+
+    return re.compile(r"^[a-zA-Z0-9-]{" + str(length) + "}$")
+
+
+def test_generate_random_api_key_with_default_length() -> None:
+    api_key_pattern = compile_regex_pattern(32)
     for _ in range(100):
         api_key = generate_api_key()
         assert api_key_pattern.match(api_key)
+
+
+@pytest.mark.parametrize("length", [8, 16, 32, 64, 128])
+def test_generate_random_api_key_with_custom_length(length: int) -> None:
+    api_key_pattern = compile_regex_pattern(length)
+    api_key = generate_api_key(length)
+    assert api_key_pattern.match(api_key)

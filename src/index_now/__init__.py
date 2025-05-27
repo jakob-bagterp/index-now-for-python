6ff54e2a15b6b5b1f639b2872c6d87f5d259a701
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-28 00:09:41 +0200

    Added filter methods to root of project, so users can import them from index_now, and grouping of __all__ variable.

--- src/index_now/__init__.py
+++ src/index_now/__init__.py
@@ -1,8 +1,15 @@
-__all__ = ["IndexNowAuthentication", "generate_api_key", "SearchEngineEndpoint", "submit_url_to_index_now", "submit_urls_to_index_now", "submit_sitemap_to_index_now", "submit_sitemaps_to_index_now"]
+__all__ = ["IndexNowAuthentication", "SearchEngineEndpoint",
+           "submit_url_to_index_now", "submit_urls_to_index_now",
+           "submit_sitemap_to_index_now", "submit_sitemaps_to_index_now",
+           "SitemapFilter", "ChangeFrequency", "DateRange",
+           "generate_api_key"]
 
 from .api_key import generate_api_key
 from .authentication import IndexNowAuthentication
 from .endpoint import SearchEngineEndpoint
+from .sitemap.filter.change_frequency import ChangeFrequency
+from .sitemap.filter.date_range import DateRange
+from .sitemap.filter.sitemap import SitemapFilter
 from .sitemap.submit import (submit_sitemap_to_index_now,
                              submit_sitemaps_to_index_now)
 from .url.submit import submit_url_to_index_now, submit_urls_to_index_now

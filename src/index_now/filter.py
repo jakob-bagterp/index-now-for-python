3e8be77658c87d7849480e62531f111442589b99
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-27 22:34:36 +0200

    Reordered change_frequency argument because it should go first when parsing the sitemap and before the URLs are extracted.

--- src/index_now/filter.py
+++ src/index_now/filter.py
@@ -21,9 +21,9 @@ class SitemapFilter:
     """Configuration class for filtering sitemap URLs based text, change frequency, date, and more.
 
     Attributes:
+        change_frequency (ChangeFrequency | None): Optional filter for URLs based on change frequency. Ignored by default or if set to `None`.
         contains (str | None): Optional filter for URLs. Can be simple string (e.g. `"section1"`) or regular expression (e.g. `r"(section1)|(section2)"`). Ignored by default or if set to `None`.
         excludes (str | None): Optional filter for URLs. Can be simple string (e.g. `"not-include-this"`) or regular expression (e.g. `r"(not-include-this)|(not-include-that)"`). Ignored by default or if set to `None`.
-        change_frequency (ChangeFrequency | None): Optional filter for URLs based on change frequency. Ignored by default or if set to `None`.
         day (datetime | None): Optional filter for URLs based on a specific date. Ignored by default or if set to `None`.
         days_ago (int | None): Optional filter for URLs based on a number of days ago. Ignored by default or if set to `None`.
         later_than (datetime | None): Optional filter for URLs based on a date range. Ignored by default or if set to `None`.
@@ -32,9 +32,9 @@ class SitemapFilter:
         take (int | None): Optional limit of URLs to be taken. Ignored by default or if set to `None`.
     """
 
+    change_frequency: ChangeFrequency | None = None
     contains: str | None = None
     excludes: str | None = None
-    change_frequency: ChangeFrequency | None = None
     day: datetime | None = None
     days_ago: int | None = None
     later_than: datetime | None = None

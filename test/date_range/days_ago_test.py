ca7728900b271a2476ad71ec67442fa7f9a3e8e3
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-28 19:03:44 +0200

    Fixed naming of test method.

--- test/date_range/days_ago_test.py
+++ test/date_range/days_ago_test.py
@@ -19,6 +19,6 @@ from index_now.sitemap.filter.date_range import DaysAgo
     (2, datetime.today() - timedelta(days=3), False),
     (2, datetime(2023, 1, 1), False),
 ])
-def test_date_range_today(days_ago: int, date_to_check: datetime, expected: bool) -> None:
+def test_date_range_days_ago(days_ago: int, date_to_check: datetime, expected: bool) -> None:
     date_range = DaysAgo(days_ago)
     assert date_range.is_within_range(date_to_check) == expected

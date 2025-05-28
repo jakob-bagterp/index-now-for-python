2a75d1743ab4872d00522f327cc1b0b64c79a17d
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-28 20:20:07 +0200

    Fixed spelling error in ChangeFrequency class values.

--- src/index_now/sitemap/filter/change_frequency.py
+++ src/index_now/sitemap/filter/change_frequency.py
@@ -5,7 +5,7 @@ from enum import Enum, unique
 class ChangeFrequency(Enum):
     """The change frequency of a sitemap URL, e.g. `<changefreq>monthly</changefreq>`, and is used to indicate to a crawler how often the resource is expected to change. Reference: https://www.sitemaps.org/protocol.html#xmlTagDefinitions"""
 
-    ALWYAS = "always"
+    ALWAYS = "always"
     HOURLY = "hourly"
     DAILY = "daily"
     WEEKLY = "weekly"

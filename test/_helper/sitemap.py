e1726867ff9699376c43a35050ea013d40cd363d
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-29 17:57:59 +0200

    WIP: Updated tests and adding inconsistent sitemap.

--- test/_helper/sitemap.py
+++ test/_helper/sitemap.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 SITEMAP_FILE_PATH = Path(__file__).parent.parent / "_mock_data" / "sitemap.xml"
 SITEMAP_ONLY_URLS_FILE_PATH = Path(__file__).parent.parent / "_mock_data" / "sitemap_only_urls.xml"
+SITEMAP_INCONSISTENT_FILE_PATH = Path(__file__).parent.parent / "_mock_data" / "sitemap_inconsistent.xml"
 
 
 def get_mock_sitemap_content() -> str:
@@ -12,5 +13,11 @@ def get_mock_sitemap_content() -> str:
 
 def get_mock_sitemap_only_urls_content() -> str:
     with open(SITEMAP_ONLY_URLS_FILE_PATH) as file:
+        content = file.read()
+    return content
+
+
+def get_mock_sitemap_inconsistent_content() -> str:
+    with open(SITEMAP_INCONSISTENT_FILE_PATH) as file:
         content = file.read()
     return content

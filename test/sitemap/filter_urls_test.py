1babb7a1befe226687d39df6fd1777e647cf770e
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-29 13:12:00 +0200

    Add date ranges to sitemap tests.

--- test/sitemap/filter_urls_test.py
+++ test/sitemap/filter_urls_test.py
@@ -1,7 +1,11 @@
+from datetime import datetime
+
 import pytest
 from _helper.sitemap import get_mock_sitemap_content
 
-from index_now import ChangeFrequency, SitemapFilter
+from index_now import (Between, ChangeFrequency, DateRange, DaysAgo,
+                       EarlierThan, EarlierThanAndIncluding, LaterThan,
+                       LaterThanAndIncluding, SitemapFilter, Today, Yesterday)
 from index_now.sitemap.filter.sitemap import SitemapUrl, filter_sitemap_urls
 from index_now.sitemap.parse import parse_sitemap_xml_and_get_urls_as_elements
 
@@ -21,12 +25,22 @@ SITEMAP_URLS_LOC = [url.loc for url in SITEMAP_URLS]
     (SITEMAP_URLS, SitemapFilter(contains=r"(page1)|(section1)"), [SITEMAP_URLS_LOC[i] for i in [1, 5, 6, 7]]),
     (SITEMAP_URLS, SitemapFilter(contains=r"(page1)|(section1)", skip=1, take=2), [SITEMAP_URLS_LOC[i] for i in [5, 6]]),
     (SITEMAP_URLS, SitemapFilter(contains="no-matches-at-all"), []),
-    (SITEMAP_URLS, SitemapFilter(excludes="page"), [SITEMAP_URLS_LOC[i] for i in [0]]),
+    (SITEMAP_URLS, SitemapFilter(excludes="page"), SITEMAP_URLS_LOC[0:1]),
     (SITEMAP_URLS, SitemapFilter(excludes="/page"), [SITEMAP_URLS_LOC[i] for i in [0, 5, 6, 7, 8]]),
     (SITEMAP_URLS, SitemapFilter(excludes=r"(page1)|(section?\d)"), [SITEMAP_URLS_LOC[i] for i in [0, 2, 3, 4]]),
     (SITEMAP_URLS, SitemapFilter(excludes="subpage"), [SITEMAP_URLS_LOC[i] for i in [0, 1, 2, 3, 4]]),
     (SITEMAP_URLS, SitemapFilter(change_frequency=ChangeFrequency.DAILY), [SITEMAP_URLS_LOC[i] for i in [2, 7]]),
     (SITEMAP_URLS, SitemapFilter(change_frequency="daily"), [SITEMAP_URLS_LOC[i] for i in [2, 7]]),
+    (SITEMAP_URLS, SitemapFilter(date_range=DateRange(start=datetime(2025, 3, 15), end=datetime(2025, 3, 20))), SITEMAP_URLS_LOC[8:]),
+    (SITEMAP_URLS, SitemapFilter(date_range=Between(datetime(2025, 1, 20), datetime(2025, 2, 20))), SITEMAP_URLS_LOC[3:5]),
+    (SITEMAP_URLS, SitemapFilter(date_range=Today()), []),
+    (SITEMAP_URLS, SitemapFilter(date_range=Yesterday()), []),
+    (SITEMAP_URLS, SitemapFilter(date_range=DaysAgo(2)), []),
+    (SITEMAP_URLS, SitemapFilter(date_range=DaysAgo((datetime.today() - datetime(2025, 3, 15)).days)), SITEMAP_URLS_LOC[8:]),
+    (SITEMAP_URLS, SitemapFilter(date_range=LaterThan(datetime(2025, 3, 10))), SITEMAP_URLS_LOC[8:]),
+    (SITEMAP_URLS, SitemapFilter(date_range=LaterThanAndIncluding(datetime(2025, 3, 10))), SITEMAP_URLS_LOC[7:]),
+    (SITEMAP_URLS, SitemapFilter(date_range=EarlierThan(datetime(2025, 1, 20))), SITEMAP_URLS_LOC[0:2]),
+    (SITEMAP_URLS, SitemapFilter(date_range=EarlierThanAndIncluding(datetime(2025, 1, 20))), SITEMAP_URLS_LOC[0:3]),
     ([], SitemapFilter(), []),
 ])
 def test_filter_sitemap_urls(sitemap_urls: list[SitemapUrl], filter: SitemapFilter, expected: list[str]) -> None:

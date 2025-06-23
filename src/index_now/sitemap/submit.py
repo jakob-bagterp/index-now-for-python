c0a4048c20d883e0c6db19d81d1d925ed97dad30
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-06-24 01:56:08 +0200

    Refactored status codes into classes with key, value pairs instead of constants.

--- src/index_now/sitemap/submit.py
+++ src/index_now/sitemap/submit.py
@@ -1,8 +1,9 @@
+import requests
 from colorist import Color
 
 from ..authentication import IndexNowAuthentication
 from ..endpoint import SearchEngineEndpoint
-from ..status_code import UNPROCESSABLE_CONTENT_STATUS_CODE
+from ..status_code import StatusCodes
 from ..url.submit import submit_urls_to_index_now
 from .filter.sitemap import SitemapFilter, filter_sitemap_urls
 from .get import get_sitemap_xml
@@ -108,21 +109,25 @@ def submit_sitemap_to_index_now(authentication: IndexNowAuthentication, sitemap_
     """
 
     urls: list[str] = []
+    response = requests.get(sitemap_location)
+    if response.status_code != 200:
+        print(f"{Color.YELLOW}Failure. No URLs found in the sitemap. Please check the sitemap location: {sitemap_location}{Color.OFF}")
+        return StatusCodes.UNPROCESSABLE_CONTENT.code
     sitemap_xml = get_sitemap_xml(sitemap_location)
     if not filter:
         urls = parse_sitemap_xml_and_get_urls(sitemap_xml)
         if not urls:
             print(f"{Color.YELLOW}No URLs found in the sitemap. Please check the sitemap location: {sitemap_location}{Color.OFF}")
-            return UNPROCESSABLE_CONTENT_STATUS_CODE
+            return StatusCodes.UNPROCESSABLE_CONTENT.code
     else:
         url_elements = parse_sitemap_xml_and_get_urls_as_elements(sitemap_xml)
         urls = filter_sitemap_urls(url_elements, filter)
         if not urls:
             print(f"{Color.YELLOW}No URLs left after filtering. Please check your filter parameters.{Color.OFF}")
-            return UNPROCESSABLE_CONTENT_STATUS_CODE
+            return StatusCodes.UNPROCESSABLE_CONTENT
     if not urls:
         print(f"{Color.YELLOW}No URLs found in the sitemap. Please check the sitemap location: {sitemap_location}{Color.OFF}")
-        return UNPROCESSABLE_CONTENT_STATUS_CODE
+        return StatusCodes.UNPROCESSABLE_CONTENT.code
     else:
         print(f"Found {Color.GREEN}{len(urls):,} URL(s){Color.OFF} in total from this sitemap: {sitemap_location}")
         status_code = submit_urls_to_index_now(authentication, urls, endpoint)
@@ -245,10 +250,10 @@ def submit_sitemaps_to_index_now(authentication: IndexNowAuthentication, sitemap
             merged_urls.extend(filter_sitemap_urls(url_elements, filter))
             if not merged_urls:
                 print(f"{Color.YELLOW}No URLs left after filtering. Please check your filter parameters.{Color.OFF}")
-                return UNPROCESSABLE_CONTENT_STATUS_CODE
+                return StatusCodes.UNPROCESSABLE_CONTENT.code
     if not merged_urls:
         print(f"{Color.YELLOW}No URLs found in the sitemaps. Please check the sitemap locations: {', '.join(sitemap_locations)}{Color.OFF}")
-        return UNPROCESSABLE_CONTENT_STATUS_CODE
+        return StatusCodes.UNPROCESSABLE_CONTENT.code
     else:
         print(f"Found {Color.GREEN}{len(merged_urls):,} URL(s){Color.OFF} in total from these sitemaps: {', '.join(sitemap_locations)}")
         status_code = submit_urls_to_index_now(authentication, merged_urls, endpoint)
--- src/index_now/status_code.py
+++ src/index_now/status_code.py
@@ -1,5 +1,43 @@
-UNPROCESSABLE_CONTENT_STATUS_CODE = 422
+from dataclasses import dataclass
+from enum import Enum
 
-SUCCESS_STATUS_CODES_DICT = {200: "OK", 202: "Accepted"}
 
-SUCCESS_STATUS_CODES = [status_code for status_code in SUCCESS_STATUS_CODES_DICT.keys()]
+@dataclass(slots=True, frozen=True)
+class StatusCode:
+    value: int
+    response: str
+
+    def __str__(self) -> str:
+        return f"{self.value} {self.response}"
+
+    def __repr__(self) -> str:
+        return f"{self.value} {self.response}"
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, int):
+            return self.value == other
+        elif isinstance(other, StatusCode):
+            return self.value == other.value
+        else:
+            return False
+
+    @property
+    def code(self) -> int:
+        return self.value
+
+
+class StatusCodes(Enum):
+    OK = StatusCode(value=200, response="OK")
+    ACCEPTED = StatusCode(value=202, response="Accepted")
+    UNPROCESSABLE_CONTENT = StatusCode(value=422, response="Unprocessable content")
+
+    def __str__(self) -> str:
+        return str(self.value.code)
+
+    def __repr__(self) -> str:
+        return str(self.value)
+
+
+SUCCESS_STATUS_CODES_DICT = {status_code.value.code: status_code.value.response for status_code in StatusCodes if str(status_code.value.code).startswith("2")}
+
+SUCCESS_STATUS_CODES = list(SUCCESS_STATUS_CODES_DICT.keys())

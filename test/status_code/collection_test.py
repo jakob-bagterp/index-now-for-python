e44ac80b135bb0b6fae007b245e30288c0235dd5
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-06-24 18:31:59 +0200

    Updated test of status codes.

--- test/status_code/collection_test.py
+++ test/status_code/collection_test.py
@@ -4,9 +4,11 @@ from index_now.status_code import SUCCESS_STATUS_CODES_DICT, StatusCodes
 def test_status_codes_int_enum() -> None:
     assert StatusCodes.OK == 200
     assert StatusCodes.ACCEPTED == 202
+    assert StatusCodes.NO_CONTENT == 204
     assert StatusCodes.UNPROCESSABLE_CONTENT == 422
 
 
 def test_status_codes_dict() -> None:
     assert SUCCESS_STATUS_CODES_DICT[200] == "OK"
     assert SUCCESS_STATUS_CODES_DICT[202] == "Accepted"
+    assert SUCCESS_STATUS_CODES_DICT[204] == "No content"

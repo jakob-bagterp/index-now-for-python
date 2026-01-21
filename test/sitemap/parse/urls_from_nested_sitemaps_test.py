5cdc335fa07fea448b4118426cb17650fc18f255
Jakob Bagterp <jakob_bagterp@hotmail.com>
2026-01-21 23:15:02 +0100

    Refactored and added tests to ensure behaviour of multiple, nested sitemaps.

--- test/sitemap/parse/urls_from_nested_sitemaps_test.py
+++ test/sitemap/parse/urls_from_nested_sitemaps_test.py
@@ -1,11 +1,13 @@
-from _helper.sitemap import get_mock_sitemap_with_nested_sitemaps_content
+from _helper.sitemap import (
+    get_mock_sitemap_with_only_nested_sitemaps_content,
+    get_mock_sitemap_with_url_and_nested_sitemaps_content)
 
 from index_now.sitemap.parse import (controller_parse_sitemap_xml_and_get_urls,
                                      parse_sitemap_xml_and_get_urls)
 
 
-def test_controller_parse_sitemap_xml_and_get_urls() -> None:
-    sitemap_content = get_mock_sitemap_with_nested_sitemaps_content()
+def test_controller_parse_sitemap_xml_and_get_urls_1() -> None:
+    sitemap_content = get_mock_sitemap_with_url_and_nested_sitemaps_content()
     urls = controller_parse_sitemap_xml_and_get_urls(sitemap_content, as_elements=False)
     assert len(urls) > 10
     assert urls[0] == "https://example.com/page1"
@@ -17,3 +19,17 @@ def test_controller_parse_sitemap_xml_and_get_urls() -> None:
     urls = parse_sitemap_xml_and_get_urls(sitemap_content)
     assert len(urls) == 1
     assert urls[0] == "https://example.com/page1"
+
+
+def test_controller_parse_sitemap_xml_and_get_urls_2() -> None:
+    sitemap_content = get_mock_sitemap_with_only_nested_sitemaps_content()
+    urls = controller_parse_sitemap_xml_and_get_urls(sitemap_content, as_elements=False)
+    assert len(urls) > 10
+    assert urls[0] == "https://jakob-bagterp.github.io/"
+    assert urls[1].startswith("https://jakob-bagterp.github.io/colorist")
+    assert not any(url.endswith(".xml") for url in urls)
+    assert not any(url.startswith("https://example.com") for url in urls)
+
+    # Ensure that the same sitemap does not yield any URLs when parsing it:
+    urls = parse_sitemap_xml_and_get_urls(sitemap_content)
+    assert len(urls) == 0

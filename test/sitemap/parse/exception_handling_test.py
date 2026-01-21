d15b64fb5c596052cbb3ca113ff89e84e2d4df81
Jakob Bagterp <jakob_bagterp@hotmail.com>
2026-01-21 20:11:58 +0100

    Refactored and split sitemap parser tests into separate folder.

---
+++ test/sitemap/parse/exception_handling_test.py
@@ -0,0 +1,18 @@
+from collections.abc import Callable
+from typing import Any
+
+import pytest
+from colorist import Color
+
+from index_now.sitemap.parse import (
+    parse_sitemap_xml_and_get_urls, parse_sitemap_xml_and_get_urls_as_elements)
+
+
+@pytest.mark.parametrize("parser", [
+    parse_sitemap_xml_and_get_urls,
+    parse_sitemap_xml_and_get_urls_as_elements,
+])
+def test_error_handling_of_parsing_invalid_sitemap(parser: Callable[[str | bytes | Any], list[str]], capfd: object) -> None:
+    parser("invalid sitemap content")
+    terminal_output, _ = capfd.readouterr()
+    assert f"{Color.YELLOW}Invalid sitemap format. The XML could not be parsed. Please check the location of the sitemap.{Color.OFF}" in terminal_output
--- test/sitemap/parse/urls_test.py
+++ test/sitemap/parse/urls_test.py
@@ -1,9 +1,4 @@
-from collections.abc import Callable
-from typing import Any
-
-import pytest
 from _helper.sitemap import get_mock_sitemap_content
-from colorist import Color
 
 from index_now.sitemap.parse import (
     parse_sitemap_xml_and_get_urls, parse_sitemap_xml_and_get_urls_as_elements)
@@ -36,13 +31,3 @@ def test_parse_sitemap_xml_and_get_urls_as_elements() -> None:
     assert last_url.lastmod == "2025-03-20"
     assert last_url.changefreq == "weekly"
     assert last_url.priority == 0.1
-
-
-@pytest.mark.parametrize("parser", [
-    parse_sitemap_xml_and_get_urls,
-    parse_sitemap_xml_and_get_urls_as_elements,
-])
-def test_error_handling_of_parsing_invalid_sitemap(parser: Callable[[str | bytes | Any], list[str]], capfd: object) -> None:
-    parser("invalid sitemap content")
-    terminal_output, _ = capfd.readouterr()
-    assert f"{Color.YELLOW}Invalid sitemap format. The XML could not be parsed. Please check the location of the sitemap.{Color.OFF}" in terminal_output
